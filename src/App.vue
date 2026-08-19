230df6a377d3be8825ef9168fd32d3fece62d401
neptoo <heywuq@outlook.com>
2026-08-19 17:17:41 +0800

    navbar and hero section

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
 </script>
 
 <template>
-
-  <RouterView />
+  <Navbar />
+  <hero title="Find your dream job" subtitle="Discover thousands of job opportunities across industries and build the career you've always wanted" />
 </template>
 
 <style scoped>
---
+++ src/components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="sec-gradient py-20 mb-4">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+            <div class="text-center">
+                <h1 class="font-serif text-4xl font-extrabold text-green-900 sm:text-5xl md:text-6xl">
+                    {{ title }}
+                </h1>
+                <p class="font-serif my-4 text-xl text-green-900">
+                    {{ subtitle }}
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+    title: String,
+    subtitle: {
+        type: String,
+        default: 'Find the Vue job that fits your skills and needs'
+    }
+})
+</script>
+
+<style scoped>
+.sec-gradient {
+    background-image: linear-gradient(to top, #c1dfc4 0%, #deecdd 100%);
+}
+</style>
